e8b760382e623f92afa9b08c70ad286abca101de
Elham Mobini <elhamobini@gmail.com>
2023-07-01 00:08:38 +0330

    my first next13 project add folder structure

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import "@styles/globals.css";
+import { Children } from "react";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & share AI prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="fa">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
